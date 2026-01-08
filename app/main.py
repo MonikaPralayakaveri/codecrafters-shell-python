8c9e083df1e7666333ad80a618a5e87d66d4f329
monik <monik@Monika.localdomain>
2026-01-08 13:33:36 +0000

    pwd

--- app/main.py
+++ app/main.py
@@ -30,6 +30,8 @@ def main():
                 args = str_split[1:]
                 if path:
                     subprocess.run([command_name] + args, executable = path)
+                elif command == "pwd":
+                    print(path)
                 else:
                     print(command+": "+"command not found")
     
