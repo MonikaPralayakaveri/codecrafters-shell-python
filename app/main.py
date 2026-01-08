c0206f1ca801b25c723e6ff911aa348fb73f6dcc
monik <monik@Monika.localdomain>
2026-01-08 11:20:47 +0000

    run a program

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ def main():
         else:
             command_path = shutil.which(str_split[0])
             if command_path:
-                subprocess.run([command_path],str_split[1:])
+                subprocess.run([command_path] +str_split[1:])
             else:
                 print(command+": "+"command not found")
     
