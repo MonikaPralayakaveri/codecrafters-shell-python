83cd523d903828f8e961a75c5e9dd8bbd2cbff01
monik <monik@Monika.localdomain>
2026-01-08 13:55:30 +0000

    pwd

--- app/main.py
+++ app/main.py
@@ -10,12 +10,17 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         str_split = command.split()
+        if not str_split:
+            continue
         if command == "exit":
             break
         elif str_split[0] == "echo":
             print(" ".join(str_split[1::]))
+        elif str_split[0] == "pwd":
+            print(os.getcsw())
         elif str_split[0] == "type":
-            if str_split[1] == "exit" or str_split[1] == "echo" or str_split[1] =="type" or command == "pwd":
+            builtin = ["exit", "echo","type","pwd"]
+            if str_split[1] in builtin:
                 print(str_split[1]+" is a shell builtin")
             elif shutil.which(str_split[1]):
                 path = shutil.which(str_split[1])
@@ -24,14 +29,13 @@ def main():
                 print(str_split[1]+": "+"not found")
         
         else:
-            if str_split:
-                command_name = str_split[0]
-                path = shutil.which(command_name)
-                args = str_split[1:]
-                if path:
-                    subprocess.run([command_name] + args, executable = path)
-                else:
-                    print(command+": "+"command not found")
+            command_name = str_split[0]
+            path = shutil.which(command_name)
+            args = str_split[1:]
+            if path:
+                subprocess.run([command_name] + args, executable = path)
+            else:
+                print(command+": "+"command not found")
     
 
 
