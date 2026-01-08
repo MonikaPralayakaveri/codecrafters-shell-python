b1956ec9660d572201d995ede0dfdc623a9f27ba
monik <monik@Monika.localdomain>
2026-01-08 10:31:51 +0000

    types executable

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def main():
         elif str_split[0] == "type":
             if str_split[1] == "exit" or str_split[1] == "echo" or str_split[1] =="type" :
                 print(str_split[1]+" is a shell builtin")
-            elif str_split[1] == "grep":
+            elif str_split[1] == "grep" or "cat":
                 print(str_split[1]+ " is /usr/bin/"+str_split[1])
             elif str_split[1] == "valid_command":
                 print(str_split[1]+" is /usr/local/bin/"+str_split[1])
