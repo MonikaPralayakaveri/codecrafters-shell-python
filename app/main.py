5747e7e1785991d74185e38eb3e5c828c3b7d40b
monik <monik@Monika.localdomain>
2026-01-09 11:27:17 +0000

    single quotes

--- app/main.py
+++ app/main.py
@@ -10,24 +10,32 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         str_split = command.split()
+        
         if not str_split:
             continue
         if command == "exit":
             break
+        
         elif str_split[0] == "echo":
             print(" ".join(str_split[1::]))
+            
         elif str_split[0] == "pwd":
             print(os.getcwd())
+        #for cd
         elif str_split[0] == "cd":
             if len(str_split) >1:
                 cdpath = str_split[1]
+                # Convert shortcuts like '~' into the full home directory path
                 cdpath = os.path.expanduser(cdpath)
                 try:
+                    # Tell the Kernel to move this process's 'marker' to the new location
                     os.chdir(cdpath)
                 except FileNotFoundError:
                     print(f"cd: {cdpath}: No such file or directory")
             else:
+                # Default behavior: If user just types 'cd', move to the Home folder
                 os.chdir(os.path.expanduser("~"))
+                
         elif str_split[0] == "type":
             builtin = ["exit", "echo","type","pwd","cd"]
             if str_split[1] in builtin:
@@ -39,10 +47,12 @@ def main():
                 print(str_split[1]+": "+"not found")
         
         else:
+            # grab the command name(eg., 'ls') and search the sys PATH for its file Location.
             command_name = str_split[0]
-            path = shutil.which(command_name)
-            args = str_split[1:]
+            path = shutil.which(command_name) #This looks in /usr/bin, /bin, etc.
+            args = str_split[1:] #collect everything after command name
             if path:
+                #in "child process" we pass the short name in list, full path -> executable
                 subprocess.run([command_name] + args, executable = path)
             else:
                 print(command+": "+"command not found")
