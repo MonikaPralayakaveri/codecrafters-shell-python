b845c0e9fe63bda57722866db3517a00c032982d
monik <monik@Monika.localdomain>
2026-01-09 11:48:11 +0000

    single quotes

--- app/main.py
+++ app/main.py
@@ -17,12 +17,9 @@ def main():
             break
         
         elif str_split[0] == "echo":
-            print(" ".join(str_split[1::]))
-            q = str_split[1:]
-            if q:
-                full_str = " ".join(q)
-                q_replace = full_str.replace("'","")
-                print(q_replace)
+            q = " ".join(str_split[1::])
+            q_replace = q.replace("'","").replace('"',"")
+            print(q_replace)
             
         elif str_split[0] == "pwd":
             print(os.getcwd())
